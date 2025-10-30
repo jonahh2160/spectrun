722769dc5fffac45a3ebf1d5e7cf464a3cb0407c
Dennon Wilson <todian24@gmail.com>
2025-10-29 23:20:43 -0500

    DW Color select screen WIP
    added buttons and started code for color select screen

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class Menu : MonoBehaviour
 {
@@ -12,6 +13,17 @@ public class Menu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    public GameObject HUD;
+    public Color color1;
+    public GameObject color2;
+    public GameObject color3;
+    public GameObject colorSelectButtons;
+
+    public Color slotSelected;
+
+    private Color selectedColor;
+
+    private int selectedSlot;
 
     private void OnEnable()
     {
@@ -34,17 +46,23 @@ public class Menu : MonoBehaviour
                 Pause();
             }
         }
+        if (GameIsPaused)
+        {
+            
+        }
     }
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        HUD.SetActive(true);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
     void Pause()
     {
         pauseMenuUI.SetActive(true);
+        HUD.SetActive(false);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
@@ -60,5 +78,71 @@ public class Menu : MonoBehaviour
     {
         Debug.Log("Quitting game...");
         Application.Quit();
+    }
+
+    void selectRed()
+    {
+        PlayerMovement.selectedColors[selectedSlot] = "Red";
+        colorSelectButtons.SetActive(false);
+    }
+
+    void selectBlue()
+    {
+        PlayerMovement.selectedColors[selectedSlot] = "Blue";
+        colorSelectButtons.SetActive(false);
+    }
+
+    void selectGreen()
+    {
+        PlayerMovement.selectedColors[selectedSlot] = "Green";
+        colorSelectButtons.SetActive(false);
+    }
+
+    void selectYellow()
+    {
+        PlayerMovement.selectedColors[selectedSlot] = "Yellow";
+        colorSelectButtons.SetActive(false);
+    }
+
+    void selectPurple()
+    {
+        PlayerMovement.selectedColors[selectedSlot] = "Purple";
+        colorSelectButtons.SetActive(false);
+    }
+
+    void selectOrange()
+    {
+        PlayerMovement.selectedColors[selectedSlot] = "Orange";
+        colorSelectButtons.SetActive(false);
+    }
+
+    void selectBlack()
+    {
+        PlayerMovement.selectedColors[selectedSlot] = "Black";
+        colorSelectButtons.SetActive(false);
+    }
+
+    void selectWhite()
+    {
+        PlayerMovement.selectedColors[selectedSlot] = "White";
+        colorSelectButtons.SetActive(false);
+    }
+
+    void selectColor1()
+    {
+        selectedSlot = 0;
+        colorSelectButtons.SetActive(true);
+    }
+
+    void selectColor2()
+    {
+        selectedSlot = 1;
+        colorSelectButtons.SetActive(true);
+    }
+
+    void selectColor3()
+    {
+        selectedSlot = 2;
+        colorSelectButtons.SetActive(true);
     }
 }
